3a17139549815f01a049fb7aec268a3250b39096
Alexsandr Ryabow <morboaport.ru@mail.ru>
2023-05-06 18:58:05 +0400

    feat: complete first task read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const stream = fs.createReadStream(path.join(__dirname,".\\text.txt"),{encoding:"utf-8"});
+stream.on("data",(chunk) => {
+    console.log(chunk);
+});
+stream.on("error",(e) => {
+    console.log(e);
+});
+

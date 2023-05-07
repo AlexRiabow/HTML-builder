ae08d54af54b6dc227dfe4744a6eef6e897e057c
Alexsandr Ryabow <morboaport.ru@mail.ru>
2023-05-07 14:28:19 +0400

    feat: complete task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const readLine = require("readline");
+
+const stream = fs.createWriteStream(path.join(__dirname,".\\text.txt"));
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function addText() {    
+    rl.question("Дополните новый файл текстом:",(answer) => {
+        if (answer === "exit") {
+            stream.end("");
+            rl.close();
+        }
+        else{
+            stream.write(answer + "\n");
+            addText();
+        }
+    });
+}
+addText();
+
+process.on("beforeExit",()=>{
+    console.log("\nРедактирование файла завершено");
+})

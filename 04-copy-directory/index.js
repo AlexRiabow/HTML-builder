92a5a4345f5ab0d780bda7bcdefc5ef2c58a3153
Alexsandr Ryabow <morboaport.ru@mail.ru>
2023-05-08 12:06:51 +0400

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require ("fs");
+const fsProm = require ("fs/promises");
+const path = require ("path");
 
+async function folderCopy() {
+   try{ 
+    const copyUrl = path.join(__dirname,".\\files-copy");
+    const directory = await fsProm.mkdir(copyUrl,{recursive:true});
+    const files = await fsProm.readdir(path.join(__dirname,".\\files"),{withFileTypes:true});
+    for (const file of files){
+        let dest = path.join(copyUrl,`.\\${file.name}`);
+        let src = path.join(__dirname,".\\files",`.\\${file.name}`)
+        const copy = fsProm.copyFile(src,dest);
+    }
+   }
+   catch(error){
+    console.error(error);
+   }
+}
+folderCopy();
